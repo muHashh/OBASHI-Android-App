f5be1219253d5895f8f6edbcbe05ba2cf348ceaf
Shourya Verma <2308432v@student.gla.ac.uk>
2019-10-29 23:49:13 +0000

    small error in name

--- ScannerApp/app/src/main/java/com/example/scannerapp/ui/about/AboutFragment.java
+++ ScannerApp/app/src/main/java/com/example/scannerapp/ui/about/AboutFragment.java
@@ -23,13 +23,7 @@ public class AboutFragment extends Fragment {
         aboutViewModel =
                 ViewModelProviders.of(this).get(AboutViewModel.class);
         View root = inflater.inflate(R.layout.fragment_about, container, false);
-        final TextView textView = root.findViewById(R.id.text_about);
-        aboutViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
