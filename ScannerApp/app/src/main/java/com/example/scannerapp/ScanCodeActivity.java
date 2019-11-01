463bc67a3a2f5269ab34ccf191a69f2f1078e8c9
NicholasSperry <sperrynicholas@gmail.com>
2019-11-01 18:05:32 +0000

    Added Camera permission to QR Scanner

--- ScannerApp/app/src/main/java/com/example/scannerapp/ScanCodeActivity.java
+++ ScannerApp/app/src/main/java/com/example/scannerapp/ScanCodeActivity.java
@@ -1,8 +1,14 @@
 package com.example.scannerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.scannerapp.ui.home.HomeFragment;
 import com.google.zxing.Result;
@@ -11,12 +17,17 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
+    private static final String TAG = "ScanCodeActivity";
+    private static final int REQUEST_CODE = 1;
     ZXingScannerView ScannerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ScannerView = new ZXingScannerView(this);
         setContentView(ScannerView);
+
+        verifyPermissions();
     }
 
     @Override
@@ -39,5 +50,25 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
 
         ScannerView.setResultHandler(this);
         ScannerView.startCamera();
+    }
+    
+    private void verifyPermissions() {
+        Log.d(TAG, "Verifying permissions: asking user for camera permission");
+        String[] permissions = {Manifest.permission.CAMERA};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+
+            onResume();
+        }
+        else {
+            ActivityCompat.requestPermissions(ScanCodeActivity.this,
+                    permissions, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
     }
 }
