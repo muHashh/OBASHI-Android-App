b497c9ba5e10f9010173228afddf909e4743269f
José Rodríguez <2300456r@student.gla.ac.uk>
2019-11-27 10:59:24 +0000

    Modify the file that connect to the database in the remote server so that it uses another user and has the port specified. Closes #48

--- DB documents/PHP scripts/db/db_connect.php
+++ DB documents/PHP scripts/db/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_USER', "root"); // db user
+define('DB_USER', "myUser"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "ObashiDB"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_SERVER', "localhost:3306"); // db server
  
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
  
