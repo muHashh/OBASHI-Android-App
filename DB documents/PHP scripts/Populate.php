f557d5f7c80fb3a0862c0389035ebe5c085aba6a
José Rodríguez <2300456r@student.gla.ac.uk>
2019-12-17 16:01:14 +0000

    Modify Populate.php so that it also adds connections between devices. Closes #41

--- DB documents/PHP scripts/Populate.php
+++ DB documents/PHP scripts/Populate.php
@@ -17,7 +17,7 @@ $connections = [['Lenovo ThinkPad P71', 'Main server', 'Request to mainframe'],
 
 //Add each device in $devices
 foreach($devices as $device){
-	$query = "SELECT * FROM Devices Where Name = ?";
+	$query = "SELECT * FROM Devices WHERE Name = ?";
 	$stmt = $con->prepare($query);
 	//Bind parameters
 	$stmt->bind_param("s",$device[0]);
@@ -54,7 +54,7 @@ foreach($devices as $device){
 
 //Add each data item in $data
 foreach($data as $d){
-	$query = "SELECT * FROM Data Where Name = ?";
+	$query = "SELECT * FROM Data WHERE Name = ?";
 	$stmt = $con->prepare($query);
 	//Bind parameters
 	$stmt->bind_param("s",$d);
@@ -83,6 +83,104 @@ foreach($data as $d){
 	else{
 		echo "Data with name ";
 		echo $d;
+		echo " already on database\n";
+	}
+	//Reset $stmt. Don't know why, but it needs this to work properly
+	$stmt = '';
+}
+
+//Add each connection in $connections
+foreach($connections as $connection){
+	$SenderID = -1;
+	$ReceiverID = -1;
+	$DataID_sendto = -1;
+	
+	//Get the DeviceID of the sender
+	$query = "SELECT DeviceID FROM Devices WHERE Name = ?";
+	$stmt = $con->prepare($query);
+	//Bind parameters
+	$stmt->bind_param("s",$connection[0]);
+	//Executing MySQL statement
+	$stmt->execute();
+	//Bind the fetched data to all of the attributes
+	$stmt->bind_result($DeviceID);
+	if($stmt->fetch()){
+		$SenderID = $DeviceID;
+	}
+	//Reset $stmt. Don't know why, but it needs this to work properly
+	$stmt = '';
+	
+	//Get the DeviceID of the sender
+	$query = "SELECT DeviceID FROM Devices WHERE Name = ?";
+	$stmt = $con->prepare($query);
+	//Bind parameters
+	$stmt->bind_param("s",$connection[1]);
+	//Executing MySQL statement
+	$stmt->execute();
+	//Bind the fetched data to all of the attributes
+	$stmt->bind_result($DeviceID);
+	if($stmt->fetch()){
+		$ReceiverID = $DeviceID;
+	}
+	//Reset $stmt. Don't know why, but it needs this to work properly
+	$stmt = '';
+	
+	//Get the DataID of the data item
+	$query = "SELECT DataID FROM Data WHERE Name = ?";
+	$stmt = $con->prepare($query);
+	//Bind parameters
+	$stmt->bind_param("s",$connection[2]);
+	//Executing MySQL statement
+	$stmt->execute();
+	//Bind the fetched data to all of the attributes
+	$stmt->bind_result($DataID);
+	if($stmt->fetch()){
+		$DataID_sendto = $DataID;
+	}
+	//Reset $stmt. Don't know why, but it needs this to work properly
+	$stmt = '';
+	
+	$query = "SELECT * FROM SendTo WHERE SenderID = ? AND ReceiverID = ? AND DataID = ?";
+	$stmt = $con->prepare($query);
+	//Bind parameters
+	$stmt->bind_param("iii",$SenderID, $ReceiverID, $DataID_sendto);
+	//Executing MySQL statement
+	$stmt->execute();
+	//If no connection found, insert connection
+	if(!$stmt->fetch()){
+		//Query to insert a data item
+		$query = "INSERT INTO SendTo(SenderID, ReceiverID, DataID) VALUES (?,?,?)";
+		$stmt = $con->prepare($query);
+		//Bind parameters
+		$stmt->bind_param("iii",$SenderID, $ReceiverID, $DataID_sendto);
+		//Executing MySQL statement
+		$stmt->execute();
+		if($stmt->affected_rows == 1){
+			echo "Conection with sender ";
+			echo $connection[0];
+			echo ", receiver ";
+			echo $connection[1];
+			echo " and data ";
+			echo $connection[2];
+			echo " successfully added\n";
+		}
+		else{
+			echo "Failure to add data with sender ";
+			echo $connection[0];
+			echo ", receiver ";
+			echo $connection[1];
+			echo " and data ";
+			echo $connection[2];
+			echo "\n";
+		}
+	}
+	else{
+		echo "Conection with sender ";
+		echo $connection[0];
+		echo ", receiver ";
+		echo $connection[1];
+		echo " and data ";
+		echo $connection[2];
 		echo " already on database\n";
 	}
 	//Reset $stmt. Don't know why, but it needs this to work properly
