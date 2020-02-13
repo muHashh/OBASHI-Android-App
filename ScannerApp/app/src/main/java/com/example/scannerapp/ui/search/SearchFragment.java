f2f4c20dbe658ed4a6e73fb6f9711005374785ee
José Rodríguez <2300456r@student.gla.ac.uk>
2020-02-13 12:53:01 +0000

    Create basic search fragment. Related to #74

---
+++ ScannerApp/app/src/main/java/com/example/scannerapp/ui/search/SearchFragment.java
@@ -0,0 +1,27 @@
+package com.example.scannerapp.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.scannerapp.R;
+
+public class SearchFragment extends Fragment {
+
+    private SearchViewModel searchViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        searchViewModel =
+                ViewModelProviders.of(this).get(SearchViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return root;
+    }
+}
