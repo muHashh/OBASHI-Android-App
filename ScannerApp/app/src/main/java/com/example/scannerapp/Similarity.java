fc0c7bd45c6daaafac1d45691ec106c4e3362440
José Rodríguez <2300456r@student.gla.ac.uk>
2020-02-13 11:39:33 +0000

    Fix the way similarity between a name and the query was calculated. Related to #73

--- ScannerApp/app/src/main/java/com/example/scannerapp/Similarity.java
+++ ScannerApp/app/src/main/java/com/example/scannerapp/Similarity.java
@@ -10,41 +10,39 @@ public class Similarity {
         deviceList = newDeviceList;
     }
 
-    public HashMap<Integer, Integer> getSimilar(String query){
+    public HashMap<Integer, Double> getSimilar(String query){
         HashMap<Integer, String> names = (HashMap)deviceList.clone();
         for(int key: names.keySet()){
-            String name = names.get(key);
+            String name = names.get(key).toLowerCase();
             names.put(key, name);
         }
         query = query.toLowerCase();
         HashMap<Character, Integer> queryChars = new HashMap<>();
         for(int i = 0; i < query.length(); i++){
             if (queryChars.containsKey(query.charAt(i))){
-                queryChars.put(query.charAt(i), queryChars.get(query.charAt(i)));
+                queryChars.put(query.charAt(i), queryChars.get(query.charAt(i))+1);
             }
             else{
                 queryChars.put(query.charAt(i), 1);
             }
         }
-        HashMap<Integer, Integer> mostSimilar = new HashMap<>();
+        HashMap<Integer, Double> mostSimilar = new HashMap<>();
         for(int key: names.keySet()){
             HashMap<Character, Integer> nameChars = new HashMap<>();
             String name = names.get(key);
             for(int i = 0; i < name.length(); i++){
                 if (nameChars.containsKey(name.charAt(i))){
-                    nameChars.put(name.charAt(i), nameChars.get(name.charAt(i)));
+                    nameChars.put(name.charAt(i), nameChars.get(name.charAt(i))+1);
                 }
                 else{
                     nameChars.put(name.charAt(i), 1);
                 }
             }
-            int dotProduct = 0;
-            for(char character: queryChars.keySet()){
-                if (nameChars.containsKey(character)){
-                    dotProduct += queryChars.get(character)*nameChars.get(character);
-                }
-            }
-            mostSimilar.put(key, dotProduct/(name.length()*query.length()));
+            double dotProduct = (double)dotProduct(queryChars, nameChars);
+            double queryLength = Math.sqrt((double)dotProduct(queryChars, queryChars));
+            double nameLength = Math.sqrt((double)dotProduct(nameChars, nameChars));
+            double distance = dotProduct/(nameLength*queryLength);
+            mostSimilar.put(key, distance);
         }
         for(int key: mostSimilar.keySet()){
             String name = names.get(key);
@@ -57,7 +55,7 @@ public class Similarity {
             }
             for(int i = 1; i < name.length()+1; i++){
                 for(int j = 1; j < query.length()+1; j++){
-                    if(name.charAt(i) == query.charAt(j)){
+                    if(name.charAt(i-1) == query.charAt(j-1)){
                         stringDistanceTable[i][j] = stringDistanceTable[i-1][j-1];
                     }
                     else{
@@ -72,8 +70,8 @@ public class Similarity {
                     }
                 }
             }
-            int distance = stringDistanceTable[name.length()][query.length()];
-            int largestLength = query.length();
+            double distance = stringDistanceTable[name.length()][query.length()];
+            double largestLength = query.length();
             if(name.length() > query.length()){
                 largestLength = name.length();
             }
@@ -81,6 +79,17 @@ public class Similarity {
         }
 
         return mostSimilar;
+    }
+
+    public int dotProduct(HashMap<Character, Integer> chars1,
+                          HashMap<Character, Integer> chars2) {
+        int dotProduct = 0;
+        for(char character: chars1.keySet()){
+            if (chars2.containsKey(character)){
+                dotProduct += chars1.get(character)*chars2.get(character);
+            }
+        }
+        return dotProduct;
     }
 
 }
